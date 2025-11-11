e7d41ff1680d01008b2afd4cc7a715c1416d947d
Poncho003 <alfonsomedinaalvarado@gmail.com>
2025-11-11 16:41:58 -0600

    Proyecto Spring Boot nimOS + base MongoDB

---
+++ src/test/java/mx/wok/nimos/NimosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.wok.nimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
